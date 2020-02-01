ff474d98cbaa99b13dc9747442edc9806c5dd00b
Luis Dario <luisda13@yahoo.es>
2020-01-31 19:24:56 -0600

    creacion de la clase usuarios.

---
+++ app/src/main/java/com/example/usuario/project_ia/Clases/Usuario.kt
@@ -0,0 +1,8 @@
+package com.example.usuario.project_ia.Clases
+
+// Definicion de la clase usuario
+class Usuario (val id: String, val usuario: String, val email: String, val password: String){
+
+//    Constructor de la clase
+    constructor():this("","","",""){}
+}
--- app/src/main/java/com/example/usuario/project_ia/CreateUsuario.kt
+++ app/src/main/java/com/example/usuario/project_ia/CreateUsuario.kt
@@ -5,9 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 
-import java.util.HashMap
+
 
 
 class CreateUsuario : AppCompatActivity() {
@@ -17,14 +16,6 @@ class CreateUsuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_usuario)
-
-
-
-        val btnAddUser = findViewById<Button>(R.id.btnCrearCuenta)
-        val tvUserName = findViewById<TextView>(R.id.etNombreUsuario)
-        val tvCorreo = findViewById<TextView>(R.id.etCorreoUsuario)
-        val tvPassword = findViewById<TextView>(R.id.etPassword)
-
 
     }
 
