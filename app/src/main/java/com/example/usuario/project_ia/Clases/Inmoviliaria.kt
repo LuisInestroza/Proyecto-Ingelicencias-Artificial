fa4fd8d92cdb69dae141388caf0b5e3704f09744
Luis Dario <luisda13@yahoo.es>
2020-02-01 19:33:17 -0600

    Deficion de la clase Inmoviliario

---
+++ app/src/main/java/com/example/usuario/project_ia/Clases/Inmoviliaria.kt
@@ -0,0 +1,6 @@
+package com.example.usuario.project_ia.Clases
+
+class Inmoviliaria(val id: String, val categoria: String, val precio: Int, val descripcion: String, val imagem: String) {
+
+    constructor():this("","",0,"", ""){}
+}
