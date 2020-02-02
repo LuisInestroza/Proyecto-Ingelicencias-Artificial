e069d035efb179764b7eab7ea8bf352777f31a86
Luis Dario <luisda13@yahoo.es>
2020-02-01 23:09:50 -0600

    Funcionalidad de acceder a la camara del dispositivo y mostrar las imagen en la ImagenView.

--- app/src/main/java/com/example/usuario/project_ia/AddInmoviliaria.kt
+++ app/src/main/java/com/example/usuario/project_ia/AddInmoviliaria.kt
@@ -2,8 +2,15 @@ package com.example.usuario.project_ia
 
 
 import android.app.Activity
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.ConnectivityManager
+import android.net.Uri
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -16,6 +23,7 @@ import com.google.firebase.database.FirebaseDatabase
 
 
 import kotlinx.android.synthetic.main.activity_add_inmoviliaria.*
+import java.util.jar.Manifest
 
 import kotlin.collections.HashMap
 
@@ -30,6 +38,10 @@ class AddInmoviliaria : AppCompatActivity() {
     lateinit var imageViewImagen: ImageView
     lateinit var buttonGuardarInmoviliario: Button
 
+//    Permisos para la camara
+    private val PERMISSION_CODE = 1000
+    var imagen: Uri? = null
+    private val IMAGEN_CAPTURE_CODE = 1001
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,10 +55,43 @@ class AddInmoviliaria : AppCompatActivity() {
         imageViewImagen = findViewById(R.id.ivInmueble)
         buttonGuardarInmoviliario = findViewById(R.id.btnAddInmueble)
 
+        // Verificar que haya conexión a internet
+
+        val cm = baseContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val coneccionInfomacion = cm.activeNetworkInfo
+
+        if (coneccionInfomacion != null && coneccionInfomacion.isConnected){
+
+            Toast.makeText(baseContext, "Conectado", Toast.LENGTH_SHORT).show()
+        }
+
+        else {
+            Toast.makeText(baseContext, "NO HAY CONEXIÓN A INTERNET", Toast.LENGTH_SHORT).show()
+        }
+
+
 
 //        Agregar la funcion de agregar un inmoviliario a la base de datos
         buttonGuardarInmoviliario.setOnClickListener {
             agregarInmoviliario()
+        }
+
+//        Hacer la conexion con la camara
+        btnCamara.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+
+                    val permiso = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestPermissions(permiso, PERMISSION_CODE)
+
+                }else{
+
+                    abrirCamara()
+                }
+            }else{
+
+                abrirCamara()
+            }
         }
 
     }
@@ -56,8 +101,8 @@ class AddInmoviliaria : AppCompatActivity() {
 //        Declarar variables de para trabajar
 
         val precio = editTextPrecio.text.toString().toInt()
-        val categoria = editTextCategoria.text.toString()
-        val descripcion = editTextDescripcion.text.toString()
+        val categoria = editTextCategoria.text.toString().trim()
+        val descripcion = editTextDescripcion.text.toString().trim()
         val imagen = imageViewImagen.toString()
 
 //        TODO() Implementar conxion a la camara y galeria del movil
@@ -65,9 +110,9 @@ class AddInmoviliaria : AppCompatActivity() {
 
 
 //        validar que los campos no esten vacios
-        if (precio == null || categoria.isEmpty() || descripcion.isEmpty()){
+        if (categoria.isEmpty() || descripcion.isEmpty()){
 
-            editTextPrecio.error = "Debes ingresar el precio"
+
             editTextCategoria.error = "Debes ingresar la categoria"
             editTextDescripcion.error = "Debes de ingresar la descripcion"
             return
@@ -89,13 +134,49 @@ class AddInmoviliaria : AppCompatActivity() {
 
 
 
+    }
+//     Funcion para abrir la camara del dispositivo
+    private fun abrirCamara(){
 
+    val valor = ContentValues()
+    valor.put(MediaStore.Images.Media.TITLE, "Nueva Imagen")
+    valor.put(MediaStore.Images.Media.DESCRIPTION, "De la camara")
+    imagen = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, valor)
+
+    val camara= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    camara.putExtra(MediaStore.EXTRA_OUTPUT, imagen)
+
+    startActivityForResult(camara, IMAGEN_CAPTURE_CODE)
+
+
+}
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        when(requestCode){
+            PERMISSION_CODE ->{
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+
+
+                }else{
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
     }
 
-
-
-
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK){
+            ivInmueble.setImageURI(imagen)
+        }
+    }
 
 
 
